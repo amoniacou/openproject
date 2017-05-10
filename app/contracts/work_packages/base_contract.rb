72d4c7873be6386f3dbe13c1d184914a17e116a5
Jens Ulferts <j.ulferts@finn.de>
2017-05-10 19:35:38 +0200

    defined model for contracts

--- app/contracts/work_packages/base_contract.rb
+++ app/contracts/work_packages/base_contract.rb
@@ -31,6 +31,10 @@ require 'model_contract'
 
 module WorkPackages
   class BaseContract < ::ModelContract
+    def self.model
+      WorkPackage
+    end
+
     attribute :subject
     attribute :description
     attribute :start_date, :due_date
